9730e1c9d4e15566943af3418e32606a3273e49f
Klein <klein.simon@csedge.org>
2019-03-02 16:00:16 -0500

    changes

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 $(document).ready(function(){
 // all jQuery methods go here...
 
+	$(".dance-icon").hover(onHovered, notHovered);
+	function onHovered() {
+		let str = $(this)[0].src;
+		let name = str.substring(0,str.length - 8);
+		$(this)[0].src = name + "giphy.gif";
+	}
 
+	function notHovered() {
+		let str = $(this)[0].src;
+		let name = str.substring(0, str.length - 9);
+		$(this)[0].src = name + "icon.png";
 
+	}
+	//challenge 1
+	$("#title").html("New title");
 
+	//Challenge 2
+	$("#extra-text").remove("");
 
+	//challenge 3
+	$("#logo").click(onClick);
+	function onClick () {
+		$("body").css("background-image", "url(images/bg2.jpg)");
+	}
 
+	//challenge 4
 
 
 
